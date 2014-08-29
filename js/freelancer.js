79c0b693719f91af41887ed178cbc32dac012a30
Zoltán Müllner <mullner.zoltan@gmail.com>
2014-08-29 18:37:53 +0200

    Add Sticky Footer css and js

--- js/freelancer.js
+++ js/freelancer.js
@@ -53,3 +53,22 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+// Sticky footer width dynamic footer height
+var bumpIt = function() {
+      $('body').css('margin-bottom', $('footer').height());
+    },
+    didResize = false;
+
+bumpIt();
+
+$(window).resize(function() {
+    didResize = true;
+});
+
+setInterval(function() {
+    if(didResize) {
+        didResize = false;
+        bumpIt();
+    }
+}, 250);
