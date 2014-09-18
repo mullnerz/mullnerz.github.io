13363072107406ef50a9fded6064f92c740199f0
Zoltán Müllner <mullner.zoltan@gmail.com>
2014-09-19 00:43:50 +0200

    Add forms.brace.io api for contact form

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,8 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "//forms.brace.io/zoltan@mullner.hu",
+                dataType: "json",
                 type: "POST",
                 data: {
                     name: name,
